3727f2e09c8e07837f8362962874ebab1ba94ef9
Stephano Romain <stephano.romain@gmail.com>
2018-08-19 13:01:16 +0300

    add loaders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx';
+
+  isRouterActive = false;
+
+  routerChange() {
+    this.isRouterActive = !this.isRouterActive;
+  }
 }
